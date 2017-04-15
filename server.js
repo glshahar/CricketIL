232357937ac4217fd1bf7bcc9b70f6694dab03ca
גל שחר <galsh20@gmail.com>
2017-04-15 05:18:43 +0300

    fix heroku11

--- server.js
+++ server.js
@@ -18,18 +18,6 @@ app.use(function(req, res, next){
 	next();
 });
 
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
-     // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-});
 
 /*** All routes ***/
 app.post('/checkLogin', db.checkLogin);
