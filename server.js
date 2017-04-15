fd1a4029f3a676006c473aaa2e1361f42220cde0
גל שחר <galsh20@gmail.com>
2017-04-15 06:05:49 +0300

    fix heroku18

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ var db = require('./dbController');
 var port = process.env.PORT || 3000;
 var fs = require('fs');
 var urlrouter = require('urlrouter');
+var http = http = require('http');
+
+var allowCrossDomain = function(req, res, next) {
+        // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+   next();
+}
 
 /*** Server settings ***/
 app.use('/', express.static('./public'));
@@ -20,6 +37,11 @@ app.use(function(req, res, next){
 	next();
 });
 
+app.use(express.favicon());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(allowCrossDomain);
+app.use(express.static(path.join(__dirname, 'public')));
 
 /*** All routes ***/
 app.post('/checkLogin', db.checkLogin);
