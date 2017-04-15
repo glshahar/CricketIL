c057a9b8e1ff3d77c9be4aa494e617a5b321d4c8
גל שחר <galsh20@gmail.com>
2017-04-15 16:39:03 +0300

    fix heroku26

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use('/', express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(function(req, res, next){
-	res.setHeader("Access-Control-Allow-Origin", "https://cricketown.herokuapp.com/");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "*");
 	res.header("Content-Type", "application/json");	
 	res.header('Access-Control-Allow-Credentials', true);
 	res.header('Access-Control-Allow-Methods', 'GET, POST');
