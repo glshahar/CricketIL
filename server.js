872ff3ba69641d7e44cfb76a131ae6b662829f39
גל שחר <galsh20@gmail.com>
2017-04-15 17:10:55 +0300

    fix heroku30

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var db = require('./dbController');
 var port = process.env.PORT || 3000;
 var fs = require('fs');
 // var urlrouter = require('urlrouter');
-var http = http = require('http');
+// var http = http = require('http');
 
 // var allowCrossDomain = function(req, res, next) {
 //         // Website you wish to allow to connect
@@ -24,9 +24,9 @@ var http = http = require('http');
 // }
 
 /*** Server settings ***/
-// app.use('/', express.static('./public'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended : true}));
+app.use('/', express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
