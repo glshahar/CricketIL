91247eed78e9eb15a7176d84b94c63968a91c8ae
גל שחר <galsh20@gmail.com>
2017-04-15 04:22:05 +0300

    fix heroku

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.set("Content-Type", "application/json");
+	res.set("Content-Type", "application/json");	
+	res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.set('Access-Control-Allow-Credentials', true);
 	next();
 });
 
