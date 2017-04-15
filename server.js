d31f30170344d4a983d157576f220fd51a537f40
גל שחר <galsh20@gmail.com>
2017-04-15 16:49:47 +0300

    fix heroku27

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Content-Type", "application/json");	
 	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Access-Control-Allow-Methods', 'GET, POST');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
 	next();
 });
 
