768c84d2dc9d7da5e4d1afd4818686af55620231
גל שחר <galsh20@gmail.com>
2017-04-15 16:30:19 +0300

    fix heroku23

--- server.js
+++ server.js
@@ -28,12 +28,12 @@ app.use('/', express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(function(req, res, next){
-	res.addHeader("Access-Control-Allow-Origin", "*");
-	res.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.addHeader("Content-Type", "application/json");	
-	res.addHeader('Access-Control-Allow-Credentials', true);
-	res.addHeader('Access-Control-Allow-Methods', 'GET, POST');
-	res.addHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	res.header("Access-Control-Allow-Origin", "https://cricketown.herokuapp.com/");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Content-Type", "application/json");	
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Methods', 'GET, POST');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 	next();
 });
 
